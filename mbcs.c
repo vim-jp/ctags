1158a57ded0acda612d7ab971f6419afd8a3ab56
mattn <mattn.jp@gmail.com>
2014-03-13 16:30:48 +0900

    Refactor

--- mbcs.c
+++ mbcs.c
@@ -33,30 +33,31 @@ extern boolean openConverter (char* encoding)
 
 extern boolean convertString (vString *const string)
 {
-	size_t utf8_len = 0, mbcs_len;
+	size_t utf8_len = 0, mbcs_len, utf8_size;
 	char *utf8, *mbcs, *utf8ptr;
 	if (iconv_fd == (iconv_t) -1)
 		return FALSE;
 	mbcs_len = vStringLength (string);
 	utf8_len = mbcs_len * 4;
-	utf8 = xCalloc (utf8_len, char);
+	utf8ptr = utf8 = xCalloc (utf8_len, char);
 	if (!utf8)
 		return FALSE;
-	utf8ptr = utf8;
 	mbcs = vStringValue (string);
 	if (iconv (iconv_fd, &mbcs, &mbcs_len, &utf8ptr, &utf8_len) == (size_t) -1)
 	{
 	  	eFree (utf8);
 		return FALSE;
 	}
-	utf8_len = utf8ptr - utf8;
+	utf8_size = utf8ptr - utf8;
 
 	vStringClear (string);
-	while (vStringSize (string) <= utf8_len + 1)
+	while (vStringSize (string) <= utf8_size + 1)
 		vStringAutoResize (string);
-	strcpy (vStringValue (string), utf8);
-	vStringLength (string) = utf8_len;
+	memcpy (vStringValue (string), utf8, utf8_size + 1);
+	vStringLength (string) = utf8_size;
   	eFree (utf8);
+
+	iconv (iconv_fd, NULL, NULL, &utf8ptr, &utf8_len);
 
 	return TRUE;
 }
@@ -65,6 +66,7 @@ extern void closeConverter ()
 {
 	if (iconv_fd != (iconv_t) -1)
 		iconv_close(iconv_fd);
+	iconv_fd = (iconv_t) -1;
 }
 
 #endif	/* SUPPORT_MULTIBYTE */
